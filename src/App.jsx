ead50f5badd9e38b1f512f98aac7a032da5095a3
Gustavo <35506966+gus25888@users.noreply.github.com>
2023-11-28 02:42:25 +0000

    Changes made with copilot

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Gustavo Baeza",
+  title: "Senior Software Developer",
+  email: "gustavobaezaavello@gmail.com",
+  gitHub: "gus25888",
+  instagram: "",
+  linkedIn: "gustavo-baeza-avello-16533884",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "gus25888",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
